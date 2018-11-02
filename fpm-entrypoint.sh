1b01c0aec4fddaf87779cbc7c932930ab7247e57
Colin Hutchinson <chutchic@gmail.com>
2018-11-02 18:28:10 +0000

    feat(rhel) setup release scripts for RHEL

--- fpm-entrypoint.sh
+++ fpm-entrypoint.sh
@@ -6,7 +6,7 @@ FPM_PARAMS=""
 if [ "$RESTY_IMAGE_BASE" == "ubuntu" ]; then
   PACKAGE_TYPE="deb"
   FPM_PARAMS="-d openssl -d libpcre3 -d perl"
-  OUTPUT_FILE_SUFFIX="${RESTY_IMAGE_TAG}.all"
+  OUTPUT_FILE_SUFFIX=".${RESTY_IMAGE_TAG}.all"
 elif [ "$RESTY_IMAGE_BASE" == "centos" ]; then
   PACKAGE_TYPE="rpm"
   FPM_PARAMS="-d pcre -d perl -d perl-Time-HiRes -d openssl"
@@ -14,6 +14,9 @@ elif [ "$RESTY_IMAGE_BASE" == "rhel" ]; then
   PACKAGE_TYPE="rpm"
   FPM_PARAMS="-d pcre -d perl -d perl-Time-HiRes -d openssl"
   OUTPUT_FILE_SUFFIX=".rhel${RESTY_IMAGE_TAG}.noarch"
+  if [ "$RESTY_IMAGE_TAG" == "7" ]; then
+    FPM_PARAMS="$FPM_PARAMS -d hostname"
+  fi
 fi
 
 fpm -a all -f -s dir \
