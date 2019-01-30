357bcb2acd1b44236df3113d2b7136a0c8fb9477
Colin Hutchinson <chutchic@gmail.com>
2019-01-30 21:23:37 +0000

    feat(tests) use taverntesting for functional testing

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set +e
+set -x
 
 kubectl apply -f https://github.com/Faithlife/minikube-registry-proxy/raw/master/kube-registry-proxy.yml
 curl -L https://github.com/Faithlife/minikube-registry-proxy/raw/master/docker-compose.yml | MINIKUBE_IP=$(minikube ip) docker-compose -p mkr -f - up -d
@@ -20,8 +21,6 @@ helm install --dep-up --name kong --set image.repository=localhost,image.tag=500
 
 while [[ "$(kubectl get deployment kong-kong | tail -n +2 | awk '{print $4}')" != 1 ]]; do
   echo "waiting for Kong to be ready"
-  kubectl get deployment kong-kong
-  kubectl get all
   sleep 10;
 done
 
@@ -31,48 +30,6 @@ ADMIN_PORT=$(kubectl get svc --namespace default kong-kong-admin -o jsonpath='{.
 echo $ADMIN_PORT
 PROXY_PORT=$(kubectl get svc --namespace default kong-kong-proxy -o jsonpath='{.spec.ports[0].nodePort}')
 echo $PROXY_PORT
-CURL_COMMAND="curl -s -o /tmp/out.txt -w %{http_code} "
-echo $CURL_COMMAND
 
-if ! [ `$CURL_COMMAND --insecure https://$HOST:$ADMIN_PORT` == "200" ]; then
-  echo "Can't invoke admin API"
-  cat /tmp/out.txt
-  exit 1
-else
-  echo "Admin API passed"
-fi
-
-RANDOM_SERVICE_NAME="randomapiname"
-RESPONSE=`$CURL_COMMAND --insecure -d "name=$RANDOM_SERVICE_NAME&url=http://mockbin.org" https://$HOST:$ADMIN_PORT/services`
-if ! [ $RESPONSE == "201" ]; then
-  echo "Can't create service"
-  cat /tmp/out.txt
-  exit 1
-else
-  echo "Created a service successfully"
-fi
-
-sleep 5
-
-SERVICE_ID=$(cat /tmp/out.txt | sed 's,^.*"id":"\([^"]*\)".*$,\1,')
-RESPONSE=`$CURL_COMMAND --insecure -d "hosts[]=$RANDOM_SERVICE_NAME.com&service.id=$SERVICE_ID" https://$HOST:$ADMIN_PORT/routes`
-if ! [ $RESPONSE == "201" ]; then
-  echo "Can't create route"
-  cat /tmp/out.txt
-  exit 1
-else
-  echo "Created a route successfully"
-fi
-
-sleep 5
-
-# Proxy Tests
-RESPONSE=`$CURL_COMMAND -H "Host: $RANDOM_SERVICE_NAME.com" http://$HOST:$PROXY_PORT/request`
-if ! [ $RESPONSE == "200" ]; then
-  echo "Can't invoke API on HTTP"
-  cat /tmp/out.txt
-  exit 1
-fi
-
-echo "Proxy and Admin smoke tests passed"
-exit 0
+docker build -t kong:test_runner -f Dockerfile.test_runner .
+docker run -it -e ADMIN_URL=${HOST}:${ADMIN_PORT} -e HOST=${HOST} -e ADMIN_PORT=${ADMIN_PORT} -e PROXY_PORT=${PROXY_PORT} kong:test_runner py.test test_smoke.tavern.yaml
