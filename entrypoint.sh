e03a0305a57e65bf533be4d790850c48ef71da04
Colin Hutchinson <chutchic@gmail.com>
2019-04-10 17:38:16 +0000

    feat(enterprise) adapt so we can build Kong or Kong Enterprise

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 ROCKS_CONFIG=$(mktemp)
 echo "
@@ -9,27 +10,66 @@ rocks_trees = {
 }
 " > $ROCKS_CONFIG
 
+export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 export LUAROCKS_CONFIG=$ROCKS_CONFIG
 export LUA_PATH="/tmp/build/usr/local/share/lua/5.1/?.lua;${BUILD}/usr/local/openresty/luajit/share/luajit-2.1.0-beta3/?.lua;;"
 export PATH=$PATH:/tmp/build/usr/local/openresty/luajit/bin
 
+if test -f /root/id_rsa; then
+  mkdir -p /root/.ssh
+  mv /root/id_rsa /root/.ssh/id_rsa
+  chmod 700 /root/.ssh/id_rsa
+fi
+
 pushd /tmp/openssl
+  make clean
+  eval ./config --prefix=/tmp/build/usr/local/kong --openssldir=/tmp/build/usr/local/kong -fPIC shared
+  make -j${RESTY_J}
+  make test
+  make install
   make install_sw
 popd
 
 pushd /tmp/yaml-${LIBYAML_VERSION}
-  make install
+  make -j${RESTY_J} \
+  && make install
 popd
 
 pushd /kong
   ROCKSPEC_VERSION=`basename /kong/kong-*.rockspec` \
-    && ROCKSPEC_VERSION=${ROCKSPEC_VERSION%.*} \
-    && ROCKSPEC_VERSION=${ROCKSPEC_VERSION#"kong-"}
+      && ROCKSPEC_VERSION=${ROCKSPEC_VERSION%.*} \
+      && ROCKSPEC_VERSION=${ROCKSPEC_VERSION#"kong-"}
 
   /tmp/build/usr/local/bin/luarocks install lyaml $LYAML_VERSION \
-    YAML_LIBDIR=/tmp/build/usr/local/kong/lib \
-    YAML_INCDIR=/tmp/yaml-${LIBYAML_VERSION} \
-    CFLAGS="-L/tmp/build/usr/local/kong/lib -Wl,-rpath,/usr/local/kong/lib -O2 -fPIC"
+      YAML_LIBDIR=/tmp/build/usr/local/kong/lib \
+      YAML_INCDIR=/tmp/yaml-${LIBYAML_VERSION} \
+      CFLAGS="-L/tmp/build/usr/local/kong/lib -Wl,-rpath,/usr/local/kong/lib -O2 -fPIC"
+
+  mkdir -p /tmp/plugin
+  
+  grep git@github.com .requirements | while read -r line ; do
+      rm -rf /tmp/plugin || true
+      echo "Processing $line"
+      repo_url=$(echo $line | cut -d " " -f1)
+      echo $repo_url
+      version=$(echo $line | cut -d " " -f2)
+      git clone --branch $version --recursive $repo_url /tmp/plugin/
+      cd /tmp/plugin/
+      /tmp/build/usr/local/bin/luarocks make kong-*.rockspec OPENSSL_LIBDIR=/tmp/openssl OPENSSL_DIR=/tmp/openssl
+      cd /kong
+  done
+  
+  grep https://api.github.com .requirements | while read -r line ; do
+      rm -rf /tmp/plugin || true
+      rm -rf /tmp/release.tar.gz || true
+      echo "Processing $line"
+      github_url=$(echo $line | cut -d " " -f1)
+      asset_url=`curl $github_url?access_token=$GITHUB_ACCESSTOKEN | grep \/assets\/ | cut -d '"' -f 4`
+      curl -L -o /tmp/release.tar.gz -H 'Accept:application/octet-stream' $asset_url?access_token=$GITHUB_ACCESSTOKEN
+      tar -xzvf /tmp/release.tar.gz --directory /tmp/plugin
+      directory=$(echo $line | cut -d " " -f2)
+      mv /tmp/plugin/dist /tmp/build/usr/local/kong/$directory
+  done
 
   /tmp/build/usr/local/bin/luarocks make kong-${ROCKSPEC_VERSION}.rockspec \
     OPENSSL_LIBDIR=/tmp/openssl \
@@ -40,9 +80,13 @@ pushd /kong
   cp kong.conf.default /tmp/build/etc/kong/kong.conf.default
 popd
 
+cp /kong/COPYRIGHT /tmp/build/usr/local/kong/
 cp /kong/bin/kong /tmp/build/usr/local/bin/kong
 sed -i.bak 's@#!/usr/bin/env resty@#!/usr/bin/env /usr/local/openresty/bin/resty@g' /tmp/build/usr/local/bin/kong && \
   rm /tmp/build/usr/local/bin/kong.bak
+
+sed -i 's/\/tmp\/build//' `find /tmp/build/usr/local/bin/ -maxdepth 1 -type f`
+sed -i 's/\/tmp\/build//' `find /tmp/build/usr/local/share/lua/5.1/luarocks/ -maxdepth 1 -type f`
 
 cp -R /tmp/build/* /output/build/
 chown -R 1000:1000 /output/build/*
