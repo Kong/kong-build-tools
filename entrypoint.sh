715e47f9ea4eac76ca88f4bfa636c29a4ed377fa
Colin Hutchinson <colin@konghq.com>
2019-06-27 17:37:56 +0000

    fix(lua) this sed is no longer necessary

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,7 +67,7 @@ popd
 cp /kong/COPYRIGHT /tmp/build/usr/local/kong/
 cp /kong/bin/kong /tmp/build/usr/local/bin/kong
 sed -i 's/resty/\/usr\/local\/openresty\/bin\/resty/' /tmp/build/usr/local/bin/kong
-sed -i 's/\/tmp\/build//' `grep -l -I -r '\#\!\/tmp\/build' /tmp/build/`
+sed -i 's/\/tmp\/build//' `grep -l -I -r '\#\!\/tmp\/build' /tmp/build/` || true
 
 cp -R /tmp/build/* /output/build/
 chown -R 1000:1000 /output/*
