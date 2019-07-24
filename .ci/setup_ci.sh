d3d52f2b5aaee005e7e6659ea50b43287e332b58
Colin Hutchinson <chutchic@gmail.com>
2019-07-24 00:03:55 +0000

    fix(ci) switch travis-ci back to xenial base image

--- .ci/setup_ci.sh
+++ .ci/setup_ci.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-GO111MODULE=on go get sigs.k8s.io/kind
+curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/v0.4.0/kind-linux-amd64
+chmod +x ./kind
+mv kind $HOME/bin/
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl $HOME/bin/
