1fc5761c2a7a4365674834e66c27e1d52f0b8fc9
Guilherme Salazar <gsz@acm.org>
2020-01-14 20:00:47 -0800

    cherry-pick feat(*) split smoke tests into separate directories
    Introduce new structure for smoke tests, allowing for easily adding more
    tests along with their required setup and teardown steps.
    
    Additionally:
    - Rename `KONG_TEST_CONTAINER_NAME` to `KONG_TEST_IMAGE_NAME` to better
    represent what the name refers to
    - All tests now use HTTP ports (ssl will be added in a subsequent
    commit)
    - Move Dockerfiles into separate directories:
        * dockerfiles/ for build Dockerfiles
        * test/ for tests Dockerfiles

--- test/build_container.sh
+++ test/build_container.sh
@@ -1,26 +1,27 @@
-if docker image inspect $KONG_TEST_CONTAINER_NAME; then exit 0; fi
+if docker image inspect $KONG_TEST_IMAGE_NAME; then exit 0; fi
+
 RHEL=false
 if [ "$RESTY_IMAGE_BASE" == "alpine" ]; then
-  DOCKER_FILE="Dockerfile.alpine"
+  DOCKERFILE="Dockerfile.alpine"
 elif [ "$RESTY_IMAGE_BASE" == "ubuntu" ] || [ "$RESTY_IMAGE_BASE" == "debian" ]; then
-  DOCKER_FILE="Dockerfile.deb"
+  DOCKERFILE="Dockerfile.deb"
 elif [ "$RESTY_IMAGE_BASE" == "centos" ]; then
-  DOCKER_FILE="Dockerfile.rpm"
+  DOCKERFILE="Dockerfile.rpm"
   cp output/${KONG_PACKAGE_NAME}-${KONG_VERSION}.el${RESTY_IMAGE_TAG}.amd64.rpm output/kong.rpm
 elif [ "$RESTY_IMAGE_BASE" == "amazonlinux" ]; then
-  DOCKER_FILE="Dockerfile.rpm"
+  DOCKERFILE="Dockerfile.rpm"
   cp output/${KONG_PACKAGE_NAME}-${KONG_VERSION}.aws.amd64.rpm output/kong.rpm
 elif [ "$RESTY_IMAGE_BASE" == "rhel" ] && [ "$RESTY_IMAGE_TAG" == "6" ]; then
   cp output/${KONG_PACKAGE_NAME}-${KONG_VERSION}.rhel${RESTY_IMAGE_TAG}.amd64.rpm output/kong.rpm
   docker pull registry.access.redhat.com/ubi${RESTY_IMAGE_TAG}/ubi
   docker tag registry.access.redhat.com/ubi${RESTY_IMAGE_TAG}/ubi rhel:${RESTY_IMAGE_TAG}
-  DOCKER_FILE="Dockerfile.rpm"
+  DOCKERFILE="Dockerfile.rpm"
   RHEL=true
 elif [ "$RESTY_IMAGE_BASE" == "rhel" ]; then
   cp output/${KONG_PACKAGE_NAME}-${KONG_VERSION}.rhel${RESTY_IMAGE_TAG}.amd64.rpm output/kong.rpm
   docker pull registry.access.redhat.com/ubi${RESTY_IMAGE_TAG}/ubi
   docker tag registry.access.redhat.com/ubi${RESTY_IMAGE_TAG}/ubi rhel:${RESTY_IMAGE_TAG}
-  DOCKER_FILE="Dockerfile.rpm"
+  DOCKERFILE="Dockerfile.rpm"
   RHEL=true
 elif [ "$RESTY_IMAGE_BASE" == "src" ]; then
   exit 0
@@ -37,5 +38,5 @@ docker build \
 --build-arg RHEL=$RHEL \
 --build-arg REDHAT_USERNAME=$REDHAT_USERNAME \
 --build-arg REDHAT_PASSWORD=$REDHAT_PASSWORD \
--f test/$DOCKER_FILE \
--t $KONG_TEST_CONTAINER_NAME .
+-f test/$DOCKERFILE \
+-t $KONG_TEST_IMAGE_NAME .
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-set -e
-set -x
+source test/util.sh
+
+if $VERBOSE; then
+  set -x
+fi
 
 if [[ "$RESTY_IMAGE_BASE" == "src" ]]; then
   exit 0
@@ -10,68 +13,21 @@ fi
 USE_TTY="-t"
 test -t 1 && USE_TTY="-it"
 
-if [[ "$RESTY_IMAGE_BASE" == "ubuntu" ]]; then
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "openresty -v" | grep -q ${RESTY_VERSION}
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "/usr/local/kong/bin/openssl version" | grep -q ${RESTY_OPENSSL_VERSION}
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "luarocks --version" | grep -q ${RESTY_LUAROCKS_VERSION}
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "luarocks config" | grep -q "/usr/local/openresty/luajit/bin/luajit"
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "luarocks config" | grep -q "/usr/local/openresty/luajit/include/luajit-2.1"
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "luarocks config" | grep -q "/usr/local/openresty/luajit/lib"
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "ldd /usr/local/openresty/bin//openresty" | grep -q "/usr/local/kong/lib/libssl.so.1.1"
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "ldd /usr/local/openresty/bin//openresty" | grep -q "/usr/local/kong/lib/libcrypto.so.1.1"
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "ldd /usr/local/openresty/bin//openresty" | grep -q "/usr/local/openresty/luajit/lib/libluajit-5.1.so.2"
-    docker run ${USE_TTY} --rm kong/kong-build-tools:test /bin/bash -c "openresty -V" | grep "/work/pcre-${RESTY_PCRE_VERSION}"
-fi
+for dir in test/tests/*; do
+  msg_test "Running '$dir' tests..."
+  msg_test "======================================================================"
 
-if [[ "$RESTY_IMAGE_TAG" == "bionic" ]]; then
-    cp output/*.deb kong.deb
-    docker run -d --rm --name systemd-ubuntu --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $PWD:/src jrei/systemd-ubuntu
-    docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "dpkg -i /src/kong.deb || true"
-    docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "apt-get update"
-    docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "apt-get install -f -y"
-    docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "systemctl start kong"
-    docker stop systemd-ubuntu
-fi
+  pushd $dir
+    test -f up.sh && source ./up.sh
 
-docker run ${USE_TTY} --rm ${KONG_TEST_CONTAINER_NAME} /bin/sh -c "luarocks --version"
-docker run ${USE_TTY} --rm ${KONG_TEST_CONTAINER_NAME} /bin/sh -c "luarocks install version"
-docker run ${USE_TTY} --rm ${KONG_TEST_CONTAINER_NAME} /bin/sh -c "ls -la /usr/local/bin/go-pluginserver"
+    set -e
+    test -f run.sh && source ./run.sh
+    set +e
 
-export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
+    test -f down.sh && source ./down.sh
+  popd
 
-while [[ "$(kubectl get pod --all-namespaces | grep -v Running | grep -v Completed | wc -l)" != 1 ]]; do
-  kubectl get pod --all-namespaces -o wide
-  echo "waiting for K8s to be ready"
-  sleep 10;
+  msg_green "=================================OK=================================="
 done
 
-kind load docker-image ${KONG_TEST_CONTAINER_NAME}
 
-helm init --wait
-helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
-helm repo update
-
-while [[ "$(kubectl get pod --all-namespaces | grep -v Running | grep -v Completed | wc -l)" != 1 ]]; do
-  kubectl get pod --all-namespaces -o wide
-  echo "waiting for K8s to be ready"
-  sleep 10;
-done
-
-helm install --dep-up --version 0.14.2 --name kong --set image.repository=localhost,image.tag=${KONG_TEST_CONTAINER_TAG} stable/kong
-
-while [[ "$(kubectl get deployment kong-kong | tail -n +2 | awk '{print $4}')" != 1 ]]; do
-  echo "waiting for Kong to be ready"
-  kubectl get pod --all-namespaces -o wide
-  sleep 10;
-done
-
-HOST="$(kubectl get nodes --namespace default -o jsonpath='{.items[0].status.addresses[0].address}')"
-echo $HOST
-ADMIN_PORT=$(kubectl get svc --namespace default kong-kong-admin -o jsonpath='{.spec.ports[0].nodePort}')
-echo $ADMIN_PORT
-PROXY_PORT=$(kubectl get svc --namespace default kong-kong-proxy -o jsonpath='{.spec.ports[0].nodePort}')
-echo $PROXY_PORT
-
-curl --insecure https://$HOST:$ADMIN_PORT/plugins -d name=kubernetes-sidecar-injector -d config.image=${KONG_TEST_CONTAINER_NAME}
-
-TEST_ADMIN_URI=https://$HOST:$ADMIN_PORT TEST_PROXY_URI=http://$HOST:$PROXY_PORT make -f Makefile run_tests
