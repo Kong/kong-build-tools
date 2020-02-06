9251b62704325edcde671c2c660b1aa3cc249cdc
Colin Hutchinson <chutchic@gmail.com>
2020-02-06 22:41:20 +0000

    test(systemctl) test all systemctl actions

--- test/tests/01-package/run.sh
+++ test/tests/01-package/run.sh
@@ -20,11 +20,20 @@ docker run ${USE_TTY} --user=root --rm ${KONG_TEST_IMAGE_NAME} /bin/sh -c "ls -l
 # TODO enable this test in other distros containing systemd
 if [[ "$RESTY_IMAGE_TAG" == "bionic" ]]; then
   cp $PACKAGE_LOCATION/*.deb kong.deb
-  docker run -d --rm --name systemd-ubuntu --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $PWD:/src jrei/systemd-ubuntu
+  docker run -d --rm --name systemd-ubuntu -e KONG_DATABASE=off --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $PWD:/src jrei/systemd-ubuntu
   docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "dpkg -i /src/kong.deb || true"
   docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "apt-get update"
   docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "apt-get install -f -y"
+  docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "mkdir -p /etc/systemd/system/kong.service.d/"
+  docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "cat <<\EOD > /etc/systemd/system/kong.service.d/override.conf
+[Service]
+Environment=KONG_DATABASE=off
+EOD"
+  docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "systemctl daemon-reload"
   docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "systemctl start kong"
+  docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "systemctl reload kong"
+  docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "systemctl restart kong"
+  docker exec ${USE_TTY} systemd-ubuntu /bin/bash -c "systemctl stop kong"
   docker stop systemd-ubuntu
   rm kong.deb
 fi
