c47542267082ae58c09c334623d6464be211965c
Colin Hutchinson <chutchic@gmail.com>
2020-05-06 14:33:49 +0000

    Revert "chore(docker-kong) adjust Kong-build-tools to be compatible with docker-kong"
    This reverts commit 1a65c6e6473d51fc87467893ea503d5eec25ef4e.

--- test/build_container.sh
+++ test/build_container.sh
@@ -6,29 +6,28 @@ rm -rf docker-kong || true
 git clone --single-branch --branch $DOCKER_KONG_VERSION https://github.com/Kong/docker-kong.git docker-kong
 
 if [ "$RESTY_IMAGE_BASE" == "ubuntu" ] || [ "$RESTY_IMAGE_BASE" == "debian" ]; then
-  cp output/*${RESTY_IMAGE_TAG}.amd64.deb docker-kong/ubuntu/empty-kong.deb
+  cp output/*${RESTY_IMAGE_TAG}.amd64.deb docker-kong/ubuntu/kong
   BUILD_DIR="ubuntu"
 elif [ "$RESTY_IMAGE_BASE" == "alpine" ]; then
-  cp output/*.amd64.apk.tar.gz docker-kong/alpine/empty-kong.tar.gz
+  cp output/*.amd64.apk.tar.gz docker-kong/alpine/kong
   BUILD_DIR="alpine"
 elif [ "$RESTY_IMAGE_BASE" == "centos" ] || [ "$RESTY_IMAGE_BASE" == "amazonlinux" ]; then
-  cp output/*.amd64.rpm docker-kong/centos/empty-kong.rpm
+  cp output/*.amd64.rpm docker-kong/centos/kong
   BUILD_DIR="centos"
 elif [ "$RESTY_IMAGE_BASE" == "rhel" ]; then
-  cp output/*.rhel${RESTY_IMAGE_TAG}.amd64.rpm docker-kong/rhel/empty-kong.rpm
+  cp output/*.rhel${RESTY_IMAGE_TAG}.amd64.rpm docker-kong/rhel/kong
   BUILD_DIR="rhel"
 else
   exit 1
 fi
 
-sed -i 's/^FROM .*/FROM '${RESTY_IMAGE_BASE}:${RESTY_IMAGE_TAG}'/' docker-kong/${BUILD_DIR}/Dockerfile
-
-exit 123
-
 pushd docker-kong/${BUILD_DIR}
     docker build -t $KONG_TEST_IMAGE_NAME \
     --no-cache \
-    --build-arg ASSET=local .
+    --build-arg LOCAL_KONG_PACKAGE=kong \
+    --build-arg RESTY_IMAGE_BASE=$RESTY_IMAGE_BASE \
+    --build-arg RESTY_IMAGE_TAG=$RESTY_IMAGE_TAG \
+    --build-arg ASSET_LOCATION=local .
 popd
 
 rm -rf docker-kong || true
