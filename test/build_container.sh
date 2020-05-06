7d77234bbaa37786dbfa207513260699e3e9b4fb
Colin Hutchinson <chutchic@gmail.com>
2020-05-06 15:05:06 +0000

    chore(docker-kong) redhat compatibility

--- test/build_container.sh
+++ test/build_container.sh
@@ -5,8 +5,6 @@ if docker image inspect $KONG_TEST_IMAGE_NAME; then exit 0; fi
 rm -rf docker-kong || true
 git clone --single-branch --branch $DOCKER_KONG_VERSION https://github.com/Kong/docker-kong.git docker-kong
 
-sed -i 's/^FROM .*/FROM '${RESTY_IMAGE_BASE}:${RESTY_IMAGE_TAG}'/' docker-kong/${BUILD_DIR}/Dockerfile
-
 if [ "$RESTY_IMAGE_BASE" == "ubuntu" ] || [ "$RESTY_IMAGE_BASE" == "debian" ]; then
   cp output/*${RESTY_IMAGE_TAG}.amd64.deb docker-kong/ubuntu/empty-kong.deb
   BUILD_DIR="ubuntu"
@@ -16,13 +14,15 @@ elif [ "$RESTY_IMAGE_BASE" == "alpine" ]; then
 elif [ "$RESTY_IMAGE_BASE" == "centos" ] || [ "$RESTY_IMAGE_BASE" == "amazonlinux" ]; then
   cp output/*.amd64.rpm docker-kong/centos/empty-kong.rpm
   BUILD_DIR="centos"
-elif [ "$RESTY_IMAGE_BASE" == "rhel" ]; then
-  sed -i 's/^FROM .*/FROM registry.access.redhat.com/ubi'${RESTY_IMAGE_TAG}'/ubi/' docker-kong/rhel/Dockerfile
+fi
+
+if [ "$RESTY_IMAGE_BASE" == "rhel" ]; then
+  sed -i 's/^FROM .*/FROM registry.access.redhat.com\/ubi'${RESTY_IMAGE_TAG}'\/ubi/' docker-kong/rhel/Dockerfile
   sed -i 's/rhel7/rhel'${RESTY_IMAGE_TAG}'/' docker-kong/rhel/Dockerfile
   cp output/*.rhel${RESTY_IMAGE_TAG}.amd64.rpm docker-kong/rhel/empty-kong.rpm
   BUILD_DIR="rhel"
 else
-  exit 1
+  sed -i 's/^FROM .*/FROM '${RESTY_IMAGE_BASE}:${RESTY_IMAGE_TAG}'/' docker-kong/${BUILD_DIR}/Dockerfile
 fi
 
 pushd docker-kong/${BUILD_DIR}
