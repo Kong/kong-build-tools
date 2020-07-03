e1dfda288c1c2553b41af3324836bfe7eb630ece
Colin Hutchinson <chutchic@gmail.com>
2020-07-03 14:06:10 +0000

    chore(docker) fix kong-build-tools w\ docker-kong compatibility

--- test/build_container.sh
+++ test/build_container.sh
@@ -6,20 +6,20 @@ rm -rf docker-kong || true
 git clone --single-branch --branch $DOCKER_KONG_VERSION https://github.com/Kong/docker-kong.git docker-kong
 
 if [ "$RESTY_IMAGE_BASE" == "ubuntu" ] || [ "$RESTY_IMAGE_BASE" == "debian" ]; then
-  cp output/*${RESTY_IMAGE_TAG}.amd64.deb docker-kong/ubuntu/empty-kong.deb
+  cp output/*${RESTY_IMAGE_TAG}.amd64.deb docker-kong/ubuntu/kong.deb
   BUILD_DIR="ubuntu"
 elif [ "$RESTY_IMAGE_BASE" == "alpine" ]; then
-  cp output/*.amd64.apk.tar.gz docker-kong/alpine/empty-kong.tar.gz
+  cp output/*.amd64.apk.tar.gz docker-kong/alpine/kong.tar.gz
   BUILD_DIR="alpine"
 elif [ "$RESTY_IMAGE_BASE" == "centos" ] || [ "$RESTY_IMAGE_BASE" == "amazonlinux" ]; then
-  cp output/*.amd64.rpm docker-kong/centos/empty-kong.rpm
+  cp output/*.amd64.rpm docker-kong/centos/kong.rpm
   BUILD_DIR="centos"
 fi
 
 if [ "$RESTY_IMAGE_BASE" == "rhel" ]; then
   sed -i 's/^FROM .*/FROM registry.access.redhat.com\/ubi'${RESTY_IMAGE_TAG}'\/ubi/' docker-kong/rhel/Dockerfile
   sed -i 's/rhel7/rhel'${RESTY_IMAGE_TAG}'/' docker-kong/rhel/Dockerfile
-  cp output/*.rhel${RESTY_IMAGE_TAG}.amd64.rpm docker-kong/rhel/empty-kong.rpm
+  cp output/*.rhel${RESTY_IMAGE_TAG}.amd64.rpm docker-kong/rhel/kong.rpm
   BUILD_DIR="rhel"
 else
   sed -i 's/^FROM .*/FROM '${RESTY_IMAGE_BASE}:${RESTY_IMAGE_TAG}'/' docker-kong/${BUILD_DIR}/Dockerfile
