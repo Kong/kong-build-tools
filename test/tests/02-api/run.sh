1ee2e3b2aec43bdf93515ba49d39cd4444f0bd6e
Guilherme Salazar <gsz@acm.org>
2021-05-05 18:32:59 -0300

    tests(*) display expected and received result
    If API test fails, code is shown.

--- test/tests/02-api/run.sh
+++ test/tests/02-api/run.sh
@@ -1,19 +1,19 @@
 set pipefail
 
-echo "Check admin API is alive"
+msg_test "Check admin API is alive"
 assert_response "$KONG_ADMIN_URI" "200"
 
-echo "Create a service"
+msg_test "Create a service"
 assert_response "-d name=testservice -d url=http://httpbin.org $KONG_ADMIN_URI/services" "201"
 
-echo  "List services"
+msg_test  "List services"
 assert_response "$KONG_ADMIN_URI/services" "200"
 
-echo "Create a route"
+msg_test "Create a route"
 assert_response "-d name=testroute -d paths=/ $KONG_ADMIN_URI/services/testservice/routes" "201"
 
-echo  "List services"
+msg_test "List services"
 assert_response "$KONG_ADMIN_URI/services" "200"
 
-echo "Proxy a request"
+msg_test "Proxy a request"
 assert_response "$KONG_PROXY_URI/anything" "200"
--- test/util.sh
+++ test/util.sh
@@ -3,26 +3,31 @@ msg_test() {
   builtin echo -en "\033[1;34m" >&1
   echo -n "===> "
   builtin echo -en "\033[1;36m" >&1
-  echo "$@"
+  echo -e "$@"
   builtin echo -en "\033[0m" >&1
 }
 
 msg_yellow() {
     builtin echo -en "\033[1;33m" >&1
-    echo "$@"
+    echo -e "$@"
     builtin echo -en "\033[0m" >&1
 }
 
 msg_green() {
   builtin echo -en "\033[1;32m" >&1
-  echo "$@"
+  echo -e "$@"
   builtin echo -en "\033[0m" >&1
 }
 
 msg_red() {
   builtin echo -en "\033[1;31m" >&2
-  echo_err "$@"
+  echo -e "$@"
   builtin echo -en "\033[0m" >&2
+}
+
+err_exit() {
+  msg_red "$@"
+  exit 1
 }
 
 start_kong() {
@@ -44,5 +49,5 @@ wait_kong() {
 
 assert_response() {
   RES=`curl -s -o /dev/null -w %{http_code} $1`
-  [ $RES == $2 ]
+  [ $RES == $2 ] || err_exit "  expected $2, got $RES"
 }
