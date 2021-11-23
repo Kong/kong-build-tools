174c65dac01da5f5ff82c4c8b9f63df65ea787e9
hutchic <chutchic@gmail.com>
2021-11-23 21:40:01 +0000

    chore(lint): shfmt against our bash scripts

--- test/build_container.sh
+++ test/build_container.sh
@@ -6,50 +6,50 @@ set -e
 DOCKER_BUILD_ARGS=()
 
 if [ "$RESTY_IMAGE_BASE" == "src" ]; then
-  exit 0
+	exit 0
 fi
 
 image_id=$(docker image inspect -f '{{.ID}}' "$KONG_TEST_IMAGE_NAME" || true)
 if [ -n "$image_id" ]; then
-  msg_test "Tests image ID: $image_id"
-  exit 0;
+	msg_test "Tests image ID: $image_id"
+	exit 0
 fi
 
 rm -rf docker-kong || true
 git clone --single-branch --branch $DOCKER_KONG_VERSION https://github.com/Kong/docker-kong.git docker-kong
 
 if [ "$RESTY_IMAGE_BASE" == "ubuntu" ] || [ "$RESTY_IMAGE_BASE" == "debian" ]; then
-  cp output/*${RESTY_IMAGE_TAG}.${ARCHITECTURE}.deb docker-kong/ubuntu/kong.deb
-  BUILD_DIR="ubuntu"
+	cp output/*${RESTY_IMAGE_TAG}.${ARCHITECTURE}.deb docker-kong/ubuntu/kong.deb
+	BUILD_DIR="ubuntu"
 elif [ "$RESTY_IMAGE_BASE" == "alpine" ]; then
-  cp output/*.${ARCHITECTURE}.apk.tar.gz docker-kong/alpine/kong.tar.gz
-  BUILD_DIR="alpine"
+	cp output/*.${ARCHITECTURE}.apk.tar.gz docker-kong/alpine/kong.tar.gz
+	BUILD_DIR="alpine"
 elif [ "$RESTY_IMAGE_BASE" == "centos" ] || [ "$RESTY_IMAGE_BASE" == "amazonlinux" ]; then
-  cp output/*.${ARCHITECTURE}.rpm docker-kong/centos/kong.rpm
-  BUILD_DIR="centos"
+	cp output/*.${ARCHITECTURE}.rpm docker-kong/centos/kong.rpm
+	BUILD_DIR="centos"
 fi
 
 if [ "$RESTY_IMAGE_TAG" == "stretch" ]; then
-  sed -i 's/apt install --yes /gdebi -n /g' docker-kong/ubuntu/Dockerfile
-  sed -i 's/unzip git/unzip git gdebi/g' docker-kong/ubuntu/Dockerfile
+	sed -i 's/apt install --yes /gdebi -n /g' docker-kong/ubuntu/Dockerfile
+	sed -i 's/unzip git/unzip git gdebi/g' docker-kong/ubuntu/Dockerfile
 fi
 
 if [ "$RESTY_IMAGE_BASE" == "rhel" ]; then
-  cp output/*.rhel${RESTY_IMAGE_TAG}.${ARCHITECTURE}.rpm docker-kong/rhel/kong.rpm
-  BUILD_DIR="rhel"
-  DOCKER_BUILD_ARGS+=(--build-arg RHEL_VERSION=$RESTY_IMAGE_TAG)
+	cp output/*.rhel${RESTY_IMAGE_TAG}.${ARCHITECTURE}.rpm docker-kong/rhel/kong.rpm
+	BUILD_DIR="rhel"
+	DOCKER_BUILD_ARGS+=(--build-arg RHEL_VERSION=$RESTY_IMAGE_TAG)
 else
-  sed -i.bak 's/^FROM .*/FROM '${RESTY_IMAGE_BASE}:${RESTY_IMAGE_TAG}'/' docker-kong/${BUILD_DIR}/Dockerfile
+	sed -i.bak 's/^FROM .*/FROM '${RESTY_IMAGE_BASE}:${RESTY_IMAGE_TAG}'/' docker-kong/${BUILD_DIR}/Dockerfile
 fi
 
 pushd docker-kong/${BUILD_DIR}
-  DOCKER_BUILD_ARGS+=(--no-cache)
-  DOCKER_BUILD_ARGS+=(--build-arg ASSET=local .)
+DOCKER_BUILD_ARGS+=(--no-cache)
+DOCKER_BUILD_ARGS+=(--build-arg ASSET=local .)
 
-  docker build -t $KONG_TEST_IMAGE_NAME \
-    "${DOCKER_BUILD_ARGS[@]}"
+docker build -t $KONG_TEST_IMAGE_NAME \
+	"${DOCKER_BUILD_ARGS[@]}"
 
-  docker run -t $KONG_TEST_IMAGE_NAME kong version
+docker run -t $KONG_TEST_IMAGE_NAME kong version
 popd
 
 rm -rf docker-kong || true
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,29 +3,29 @@
 source test/util.sh
 
 if $VERBOSE; then
-  set -x
+	set -x
 fi
 
 if [[ "$RESTY_IMAGE_BASE" == "src" ]]; then
-  exit 0
+	exit 0
 fi
 
 USE_TTY="-t"
 test -t 1 && USE_TTY="-it"
 
 for dir in test/tests/*; do
-  msg_test "Running '$dir' tests..."
-  msg_test "======================================================================"
+	msg_test "Running '$dir' tests..."
+	msg_test "======================================================================"
 
-  pushd $dir
-    test -f up.sh && source ./up.sh
+	pushd $dir
+	test -f up.sh && source ./up.sh
 
-    set -e
-    test -f run.sh && source ./run.sh
-    set +e
+	set -e
+	test -f run.sh && source ./run.sh
+	set +e
 
-    test -f down.sh && source ./down.sh
-  popd
+	test -f down.sh && source ./down.sh
+	popd
 
-  msg_green "=================================OK=================================="
+	msg_green "=================================OK=================================="
 done
