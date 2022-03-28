cc2685f3fa056ec84aaacb953e401c283c258cac
hutchic <chutchic@gmail.com>
2022-03-28 19:42:14 +0000

    fix(ci): the deb packages generate separate arch specific packages

--- test/build_container.sh
+++ test/build_container.sh
@@ -19,6 +19,8 @@ if [ "$RESTY_IMAGE_BASE" == "src" ]; then
   exit 0
 elif [ "$RESTY_IMAGE_BASE" == "alpine" ]; then
   cp output/*.apk.tar.gz docker-kong/kong.apk.tar.gz
+elif [ "$PACKAGE_TYPE" == "deb" ]; then
+  cp output/*${ARCHITECTURE}*.deb docker-kong/kong.deb
 else
   cp output/*.${PACKAGE_TYPE} docker-kong/kong.${PACKAGE_TYPE}
 fi
