f7b0f03f25ade36a20a4a3aca67978d0df4329c2
hutchic <chutchic@gmail.com>
2022-04-01 19:26:17 +0000

    chore(packaging): theses ssh keys were never used

--- build-kong.sh
+++ build-kong.sh
@@ -10,9 +10,9 @@ rocks_trees = {
 }
 " > $ROCKS_CONFIG
 
+cp -R /usr/local/share/lua/ /tmp/build/usr/local/share/ 
 cp -R /tmp/build/* /
 
-export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 export LUAROCKS_CONFIG=$ROCKS_CONFIG
 export LUA_PATH="/usr/local/share/lua/5.1/?.lua;/usr/local/openresty/luajit/share/luajit-2.1.0-beta3/?.lua;;"
 export PATH=$PATH:/usr/local/openresty/luajit/bin
@@ -21,13 +21,6 @@ export PATH=$PATH:/usr/local/openresty/luajit/bin
 /usr/local/kong/bin/openssl version
 /usr/local/openresty/bin/openresty -v
 
-if test -f /root/id_rsa; then
-  mkdir -p /root/.ssh
-  mv /root/id_rsa /root/.ssh/id_rsa
-  chmod 700 /root/.ssh/id_rsa
-  ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-fi
-
 pushd /kong
   ROCKSPEC_VERSION=`basename /kong/kong-*.rockspec` \
     && ROCKSPEC_VERSION=${ROCKSPEC_VERSION%.*} \
@@ -35,17 +28,12 @@ pushd /kong
 
   mkdir -p /tmp/plugin
   
-  luarocks purge --tree=/tmp/build/usr/local --old-versions || true
   /usr/local/bin/luarocks make kong-${ROCKSPEC_VERSION}.rockspec \
     CRYPTO_DIR=/usr/local/kong \
     OPENSSL_DIR=/usr/local/kong \
     YAML_LIBDIR=/tmp/build/usr/local/kong/lib \
     YAML_INCDIR=/tmp/yaml \
     CFLAGS="-L/tmp/build/usr/local/kong/lib -Wl,-rpath,/usr/local/kong/lib -O2 -fPIC"
-  
-  pushd /distribution
-    ./post-*.sh
-  popd
 
   mkdir -p /tmp/build/etc/kong
   cp kong.conf.default /tmp/build/usr/local/lib/luarocks/rock*/kong/$ROCKSPEC_VERSION/
