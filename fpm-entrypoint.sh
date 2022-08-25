da2c0cdd45371d0f9f3cc14b97e04bdba0c20546
Colin Hutchinson <chutchic@gmail.com>
2022-08-25 18:35:14 +0000

    fix(expect): expect is now part of kong/fpm

--- fpm-entrypoint.sh
+++ fpm-entrypoint.sh
@@ -95,7 +95,6 @@ else
   && mv kong*.* /output/${KONG_PACKAGE_NAME}-${KONG_VERSION}${OUTPUT_FILE_SUFFIX}.${PACKAGE_TYPE}
   set -x
   if [ "$PACKAGE_TYPE" == "rpm" ] && [ ! -z "$PRIVATE_KEY_PASSPHRASE" ]; then
-    apt-get install -y expect
     mkdir -p ~/.gnupg/
     touch ~/.gnupg/gpg.conf
     echo use-agent >> ~/.gnupg/gpg.conf
