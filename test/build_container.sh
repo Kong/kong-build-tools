fa4c71b938db478979f99b77ce4761550ddf99c0
Colin Hutchinson <chutchic@gmail.com>
2022-10-28 14:02:52 +0000

    fix(arm64): use qemu to do the packaging step for arm64 images refs: #ENGEN-715 (#520)
    * fix(arm64): use qemu to do the packaging step for arm64 images
    
    * chore(revert): revert me
    
    * Revert "chore(revert): revert me"
    
    This reverts commit 3913aaa3e5af8eede7dcef5bbb6991c0e5789868.
    
    * chore(src): dont do anything when we're doing a source code release
    
    * small tweaks
    
    * fix(arm64): s/BUILDPLATFORM/TARGETPLATFORM/g
    
    * docs(README): update and refresh the README.md
    
    * test(wip): run only the oss alpine build #revertme
    
    * Revert "test(wip): run only the oss alpine build #revertme"
    
    This reverts commit 17444f5d10180a5d471a65c3487c3fe9c7863f6c.
    
    * chore(fix): facepalm
    
    * fix(ci): ADDITIONAL_TAGS applies to non arm manifests as well
    
    * Update test/tests/01-package/run.sh

--- test/build_container.sh
+++ test/build_container.sh
@@ -5,6 +5,8 @@ set -e
 
 DOCKER_BUILD_ARGS=()
 
+KONG_TEST_IMAGE_NAME=$DOCKER_RELEASE_REPOSITORY:$ARCHITECTURE-$KONG_TEST_CONTAINER_TAG
+
 image_id=$(docker image inspect -f '{{.ID}}' "$KONG_TEST_IMAGE_NAME" || true)
 if [ -n "$image_id" ]; then
   msg_test "Tests image Name: $KONG_TEST_IMAGE_NAME"
@@ -19,7 +21,7 @@ chmod -R 755 docker-kong/*.sh
 if [ "$RESTY_IMAGE_BASE" == "src" ]; then
   exit 0
 elif [ "$RESTY_IMAGE_BASE" == "alpine" ]; then
-  cp output/*${ARCHITECTURE}*.apk.tar.gz docker-kong/kong.apk.tar.gz
+  cp output/${KONG_PACKAGE_NAME}-${KONG_VERSION}.${ARCHITECTURE}.apk.tar.gz docker-kong/kong.apk.tar.gz
 elif [ "$PACKAGE_TYPE" == "deb" ]; then
   cp output/*${ARCHITECTURE}*.deb docker-kong/kong.deb
 else
@@ -45,14 +47,18 @@ pushd ./docker-kong
     DOCKER_BUILD_ARGS+=(--label "org.opencontainers.image.revision=$DOCKER_LABEL_REVISION")
   fi
 
+  DOCKER_BUILD_ARGS+=(--platform linux/${ARCHITECTURE})
   DOCKER_BUILD_ARGS+=(--no-cache)
+  DOCKER_BUILD_ARGS+=(--pull)
   DOCKER_BUILD_ARGS+=(--build-arg ASSET=local .)
-
-  docker build --progress=${DOCKER_BUILD_PROGRESS:-auto} -t $KONG_TEST_IMAGE_NAME -f Dockerfile.$PACKAGE_TYPE \
+  
+  docker build \
+    --progress=${DOCKER_BUILD_PROGRESS:-auto} \
+    -t $KONG_TEST_IMAGE_NAME \
+    -f Dockerfile.$PACKAGE_TYPE \
     ${DOCKER_LABELS} \
     "${DOCKER_BUILD_ARGS[@]}"
 
-  docker run -t $KONG_TEST_IMAGE_NAME kong version
   msg_test "Tests image Name: $KONG_TEST_IMAGE_NAME"
 popd
 
